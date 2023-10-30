8220870dc6526084b5f7530deb30b39100baa1c9
parthwebdev <parth.webdev@gmail.com>
2023-10-30 18:16:32 +0530

    Added React Carousels Part 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import Carousel from "./Carousel 01/Carousel";
+import Carousel from "./components/Carousel 02/Carousel";
 
-import image1 from "./Carousel 01/imgs/Image-1.jpeg";
-import image2 from "./Carousel 01/imgs/Image-2.jpg";
-import image3 from "./Carousel 01/imgs/Image-3.jpg";
-import image4 from "./Carousel 01/imgs/Image-4.jpg";
-import image5 from "./Carousel 01/imgs/Image-5.jpg";
+import image1 from "./components/Carousel 02/assets/01.jpg";
+import image2 from "./components/Carousel 02/assets/02.jpg";
+import image3 from "./components/Carousel 02/assets/03.jpg";
+import image4 from "./components/Carousel 02/assets/04.jpg";
+import image5 from "./components/Carousel 02/assets/05.jpg";
+import image6 from "./components/Carousel 02/assets/06.jpg";
 
-const imgs = [image1, image2, image3, image4, image5];
+const imgs = [image1, image2, image3, image4, image5, image6];
 
 const App = () => {
-  return <Carousel imgs={imgs} />;
+  return <Carousel slides={imgs} />;
 };
 
 export default App;
---
+++ src/components/Carousel 02/Carousel.tsx
@@ -0,0 +1,60 @@
+import { useRef } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./styles.css";
+
+import arrowLeft from "./assets/arrow-left.svg";
+import arrowRight from "./assets/arrow-right.svg";
+
+interface Props {
+  slides: string[];
+}
+
+function Carousel({ slides }: Props) {
+  // const swiperLeft = useRef<HTMLDivElement>(null);
+  // const swiperRight = useRef<HTMLDivElement>(null);
+
+  return (
+    <Swiper
+      modules={[EffectCoverflow, Navigation, Pagination]}
+      navigation={{
+        prevEl: ".button-prev",
+        nextEl: ".button-next",
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      speed={1000}
+      slidesPerView={"auto"}
+      centeredSlides
+      effect={"coverflow"}
+      coverflowEffect={{
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      }}
+    >
+      {slides.map((slide, index) => (
+        <SwiperSlide key={index} className="slide-inner">
+          <img src={slide} alt="" />
+        </SwiperSlide>
+      ))}
+      <div className="button-prev">
+        <img src={arrowLeft} alt="Left" />
+      </div>
+      <div className="button-next">
+        <img src={arrowRight} alt="Right" />
+      </div>
+    </Swiper>
+  );
+}
+
+export default Carousel;
